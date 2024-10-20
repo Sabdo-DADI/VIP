22bafce9db8c1bfb3fbc45053442fa5e3c5ae3f7
SABDO PALON STORE <164147019+Sabdo-DADI@users.noreply.github.com>
2024-10-20 11:16:49 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -19,9 +19,34 @@ else
         echo "File '$file_path' sudah ada dan berisi data."
     fi
 fi
-# Link Hosting Kalian
-sudo apt install python3
 
+# Cek apakah python3 terinstal
+if ! command -v python3 &> /dev/null; then
+    echo "Python 3 belum terinstal. Menginstal Python 3 sekarang..."
+    sudo apt update
+    sudo apt install python3 -y
+else
+    echo "Python 3 sudah terinstal."
+fi
+
+# Cek apakah python3-pip terinstal
+if ! command -v pip3 &> /dev/null; then
+    echo "pip3 belum terinstal. Menginstal pip3 sekarang..."
+    sudo apt update
+    sudo apt install python3-pip -y
+else
+    echo "pip3 sudah terinstal."
+fi
+
+# Cek dan install requests pada Python 3
+if python3 -c "import requests" &> /dev/null; then
+    echo "Library 'requests' sudah terinstal untuk Python 3."
+else
+    echo "Library 'requests' belum terinstal, menginstal sekarang..."
+    sudo pip3 install requests
+fi
+
+# Link Hosting Kalian
 wget -O /usr/local/bin/ws "https://raw.githubusercontent.com/Sabdo-DADI/VIP/main/sshws/ws"
 chmod +x /usr/local/bin/ws
 
